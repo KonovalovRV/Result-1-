5488cbfc93ae00a712e37bdf8b984f5cdb019dc8
Roman <vvptus@yandex.ru>
2022-11-26 00:04:34 +0300

    control work

---
+++ taskresult/Program.cs
@@ -0,0 +1,19 @@
+﻿string [] mass = {"Hello","world",":-)","5"};
+
+void SortArray(string[]mas)
+ {
+  string text=string.Empty;
+  int i=0;
+  int length=mas.Length;
+  for(i=0;i<length;i++)
+   {
+        text = mas[i];
+    if (text.Length<4)
+    {
+    Console.Write ($"[{mas[i]}]  ");   
+    }
+   }
+    Console.WriteLine();   
+ }
+
+SortArray(mass);
